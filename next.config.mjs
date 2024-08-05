dbad7f74b2f7b29b42ea5ff08874782621a1a185
hahan <hanvietha141@gmail.com>
2024-08-05 16:37:40 +0700

    (+) svgr

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,60 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+		scrollRestoration: true,
+		turbo: {
+			rules: {
+				'*.svg': {
+					loaders: [{
+						loader: "@svgr/webpack",
+						options: {
+							typescript: true,
+							ext: "tsx",
+							svgoConfig: {
+								plugins: [{
+									name: 'preset-default',
+									params: {
+										overrides: {
+											removeViewBox: false,
+										},
+									},
+								},]
+							}
+						}
+					}],
+					as: '*.js'
+				}
+			}
+		}
+	},
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            typescript: true,
+            ext: "tsx",
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
